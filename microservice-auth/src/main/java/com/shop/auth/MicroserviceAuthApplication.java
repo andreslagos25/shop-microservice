b56a96fca171d448e7e83f7b3a3de464ea6f2fa6
Andres Lagos <lagosfelipe56@gmail.com>
2024-10-01 21:52:15 -0500

    Habilita el FeignClient en la aplicacion prinicipal

--- microservice-auth/src/main/java/com/shop/auth/MicroserviceAuthApplication.java
+++ microservice-auth/src/main/java/com/shop/auth/MicroserviceAuthApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 import java.util.Set;
 
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceAuthApplication {
