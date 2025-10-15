038d09bd54cc4562d80539a81e896faec0782dba
andreslagos25 <lagosfelipe56@gmail.com>
2025-10-15 17:33:21 -0500

    feature(msvc-products): - Se agrega metodo para manejar excepciones de Jakarta Validations - Se agrega decorador @Valid en controller brand para manejar excepcion

--- microservice-products/src/main/java/com/shop/products/controller/BrandController.java
+++ microservice-products/src/main/java/com/shop/products/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.shop.products.controller;
 
 import com.shop.products.controller.dto.BrandCreateRequest;
 import com.shop.products.service.impl.BrandServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class BrandController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveBrand(@RequestBody BrandCreateRequest brandCreateRequest){
+    public void saveBrand(@Valid @RequestBody BrandCreateRequest brandCreateRequest){
         brandService.saveBrand(brandCreateRequest);
     }
 }
--- microservice-products/src/main/java/com/shop/products/exception/GlobalExceptionHandler.java
+++ microservice-products/src/main/java/com/shop/products/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.shop.products.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +12,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ApiError> handleValidation(ValidationException ex){
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleJakartaValidation(MethodArgumentNotValidException ex){
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "There's a problem with one or more fields in your request");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
     }
 }
